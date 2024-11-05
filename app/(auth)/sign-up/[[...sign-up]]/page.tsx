43197ec3372e6bf38ee15b0b938e179ae0c0fc6b
leylagahramanova <leylagahramanova13@gmail.com>
2024-11-05 15:35:04 +0400

    add

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from '@clerk/nextjs'
+const SignUpPage = () => {
+    return (
+        <main className="auth-page">
+            <SignUp />
+        </main>
+    )
+}
+export default SignUpPage
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b6a1a7b23bc8df180c1b9678ac3c9143@o4507910838943744.ingest.de.sentry.io/4507910881804368",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
