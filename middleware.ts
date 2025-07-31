24eb095b3f930dda5796e613aadfb5202cb57d6f
leylagahramanova <leylagahramanova13@gmail.com>
2025-07-31 13:24:36 +0400

    mild

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,8 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
- "/((?!.*\\..*|_next).*)","/", "/(api|trpc)(.*)"
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+
+    '/(api|trpc)(.*)',
   ],
 };
